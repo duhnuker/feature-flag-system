30a578a7e14f09154027b9a86e43b32a402b7819
duhnuker <dinukanh@gmail.com>
2025-03-24 21:43:05 +1100

    updateFeatureFlag mutation

--- frontend/services/featureFlags.ts
+++ frontend/services/featureFlags.ts
@@ -3,9 +3,18 @@ import client from "./graphqlClient";
 
 const GET_FEATURE_FLAG = gql`
     query GetFeatureFlag($name: String!) {
-    featureFlag(name: $name) {
-        name
-        isEnabled
+        featureFlag(name: $name) {
+            name
+            isEnabled
+        }
+    }
+`;
+
+const UPDATE_FEATURE_FLAG = gql`
+    mutation UpdateFeatureFlag($name: String!, $isEnabled: Boolean!) {
+        updateFeatureFlag(name: $name, isEnabled: $isEnabled) {
+            name
+            isEnabled
         }
     }
 `;
@@ -18,3 +27,13 @@ export const fetchFeatureFlag = async (name: string) => {
 
     return data.featureFlag;
 };
+
+export const toggleFeatureFlag = async (name: string, isEnabled: boolean) => {
+    const { data } = await client.mutate({
+        mutation: UPDATE_FEATURE_FLAG,
+        variables: { name, isEnabled },
+    });
+
+    return data.updateFeatureFlag;
+
+}
