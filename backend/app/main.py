46d60ea891280bff06d2f800fec47ab8634f5dc8
duhnuker <dinukanh@gmail.com>
2025-03-24 16:47:18 +1100

    GraphQL setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+from app.api.graphql import schema
 
 app = FastAPI()
 
+graphql_app = GraphQLRouter(schema)
+app.include_router(graphql_app, prefix="/graphql")
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def health_check():
+    return {"status": "running"}
