6d6f8419323d1b0872b6369d288c89c1814f4d45
duhnuker <dinukanh@gmail.com>
2025-03-31 15:57:16 +1100

    graphql env

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.APOLLO_API_URL || 'http://localhost:8000/graphql',
+  },
 };
 
 export default nextConfig;
--- frontend/services/graphqlClient.ts
+++ frontend/services/graphqlClient.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const API_URL = process.env.APOLLO_API_URL || 'http://localhost:8000/graphql';
+
 const client = new ApolloClient({
-    uri: 'http://localhost:8000/graphql',
+    uri: API_URL,
     cache: new InMemoryCache(),
     defaultOptions: {
         watchQuery: {
